25d196de35d7c01ff3a0ff5445c039ab3a6454b1
Niek125 <niek.sleddens@gmail.com>
2020-02-05 11:23:40 +0100

    small refactors

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -9,18 +9,18 @@
             </v-col>
             <v-spacer></v-spacer>
             <v-col cols="4">
-                <v-autocomplete :items="projects" item-text="projectname" outlined dense shaped label="search"
+                <v-autocomplete :items="projects" item-text="projectName" outlined dense shaped label="search"
                                 :hide-details="true" :class="level1">
                     <template v-slot:label>
                         <span :class="textColor">Search</span>
                     </template>
                     <template v-slot:item="data">
                         <v-list-item-content v-text="data.item.projectname" class="black--text"
-                                             v-on:click="redirectProject(data.item.projectid, comp)"></v-list-item-content>
+                                             v-on:click="redirectProject(data.item.projectid)"></v-list-item-content>
                     </template>
                     <template v-slot:no-data>
                         <v-list-item>
-                            <v-list-item-content>No datasets found</v-list-item-content>
+                            <v-list-item-content>No projects found</v-list-item-content>
                         </v-list-item>
                     </template>
                 </v-autocomplete>
@@ -77,18 +77,17 @@
                 location.reload();
             },
             toOverview: function () {
-                if (this.$route.name === 'overview') {
-                    // location.reload();
-                    this.$router.push('/login');
+                if (this.$route.name === "overview") {
+                    location.reload();
                 } else {
-                    this.$router.push('/overview');
+                    this.$router.push("/overview");
                 }
             }
         },
         created() {
             try {
                 this.uName = this.$session.get("userData").unm;
-                this.pfp = this.$session.get('userData').pfp;
+                this.pfp = this.$session.get("userData").pfp;
             } catch (e) {
                 // alert("You are not logged in")
             }
