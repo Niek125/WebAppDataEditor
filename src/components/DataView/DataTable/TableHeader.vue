1d37103498f3d0bb2a97a5ebedf0001a3da99677
Niek125 <niek.sleddens@gmail.com>
2020-02-11 19:49:16 +0100

    fixed resizing columns

--- src/components/DataView/DataTable/TableHeader.vue
+++ src/components/DataView/DataTable/TableHeader.vue
@@ -2,24 +2,23 @@
     <thead>
     <tr>
         <th v-for="header in props.headers" :key="'header' + header.name + header.value"
-            v-on:mouseup="mouseDown = false">
-            <v-sheet :width="header.width - 72">
-                <v-row class="ma-0" align="center" justify="center">
-                    <v-col class="pa-0">
+            v-on:mouseup="mouseDown = false" class="pr-0">
+            <v-row class="ma-0">
+                <v-sheet :width="header.width" tile>
+                    <v-row class="ma-0" align="center" justify="center">
                         <v-btn icon v-on:mousedown="() => {mouseDown = true; setHeaderWidth(header, -1);}">
                             <v-icon>mdi-chevron-left</v-icon>
                         </v-btn>
-                    </v-col>
-                    <v-col class="pa-0 overflow-hidden">
-                        <v-sheet tile :width="header.width"><span>{{header.text}}</span></v-sheet>
-                    </v-col>
-                    <v-col class="pa-0">
+                        <v-sheet tile :width="header.width - (2 * buttonWidth)">
+                            <div class="text-no-wrap overflow-x-hidden">{{header.text}}</div>
+                        </v-sheet>
                         <v-btn icon v-on:mousedown="() => {mouseDown = true; setHeaderWidth(header, 1);}">
                             <v-icon>mdi-chevron-right</v-icon>
                         </v-btn>
-                    </v-col>
-                </v-row>
-            </v-sheet>
+                    </v-row>
+                </v-sheet>
+                <v-divider vertical></v-divider>
+            </v-row>
         </th>
     </tr>
     </thead>
@@ -35,11 +34,12 @@
         data() {
             return {
                 mouseDown: false,
+                buttonWidth: 36,
             }
         },
         methods: {
             setHeaderWidth: function (header, value) {
-                if (value < 0 && header.width <= 0) {
+                if (value < 0 && header.width <= 72) {
                     return;
                 }
                 header.width += value;
